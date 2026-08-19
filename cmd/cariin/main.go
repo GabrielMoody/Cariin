a2d502ebdce05137b7ef173791d3eddddd922c1e
GabrielMoody <gabrielmdy08@gmail.com>
2026-08-19 21:23:51 +0700

    feat: add inverted index for static documents data

--- cmd/cariin/main.go
+++ cmd/cariin/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/GabrielMoody/Cariin/internal/documents"
+	"github.com/GabrielMoody/Cariin/internal/index"
+)
+
+func main() {
+	idx := index.Build(documents.Documents)
+
+	fmt.Println(idx)
 }
