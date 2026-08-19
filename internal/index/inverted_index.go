405e80ad0c4d213988e045284f3fe13f8b650cbc
GabrielMoody <gabrielmdy08@gmail.com>
2026-08-19 21:25:12 +0700

    feat: add inverted index for static documents data

---
+++ internal/index/inverted_index.go
@@ -0,0 +1,57 @@
+package index
+
+import (
+	"strings"
+
+	"github.com/GabrielMoody/Cariin/internal/documents"
+)
+
+type InvertedIdx map[string][]int64
+
+func tokenize(text string) []string {
+	text = strings.ToLower(text)
+
+	replacer := strings.NewReplacer(
+		".", " ",
+		",", " ",
+		"!", " ",
+		"?", " ",
+		":", " ",
+		";", " ",
+		"(", " ",
+		")", " ",
+		"[", " ",
+		"]", " ",
+		"{", " ",
+		"}", " ",
+		"/", " ",
+		"-", " ",
+		"_", " ",
+	)
+
+	text = replacer.Replace(text)
+
+	return strings.Fields(text)
+}
+
+func Build(docs []documents.Document) *InvertedIdx {
+	idx := make(InvertedIdx)
+
+	for _, v := range docs {
+		text := v.Title + " " + v.Body
+
+		tokens := tokenize(text)
+		seen := make(map[string]bool)
+
+		for _, token := range tokens {
+			if seen[token] {
+				continue
+			}
+
+			idx[token] = append(idx[token], v.Id)
+			seen[token] = true
+		}
+	}
+
+	return &idx
+}
